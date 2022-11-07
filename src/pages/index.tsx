5e8335745316e395cbe68e8a2e96d84376474312
Alina <67075021+alinabudukina@users.noreply.github.com>
2022-11-07 23:54:02 +0100

    feat: remove unused css&html

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import { type NextPage } from "next";
+
+import { trpc } from "../utils/trpc";
+
+const Home: NextPage = () => {
+  return <div className="text-2xl">Hello world</div>;
+};
+
+export default Home;
