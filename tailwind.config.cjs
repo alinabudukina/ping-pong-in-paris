eafd5fda9b5cb1b190f9113cd9726380ca5e1e1c
Alina <67075021+alinabudukina@users.noreply.github.com>
2022-11-29 14:56:25 +0100

    add colors for dark mode

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,7 +9,10 @@ module.exports = {
     },
     colors: {
       coral: "#e74132",
-      gray: "#eff0f2",
+      gray: "#f8f8f8",
+      black: "#0e1016",
+      "dark-gray": "#151922",
+      "stone-gray": "#8a8c91",
       white: "#ffffff",
     },
   },
